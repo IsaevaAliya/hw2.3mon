827607940797c75a5fe29ee102bfcff575e746be
IsaevaAliya <aliyaisaeva5@gmail.com>
2024-10-24 21:41:29 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw23mon/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.hw23mon;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container_view, new SecondFragment()).commit();    }
+}
